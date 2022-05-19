2740a07a33a804bd56a2cbb709c32eed86b06f75
brotherstone97 <brotherstone97@gmail.com>
2022-05-19 19:38:48 +0900

    Fix: datetime field(DateFiled -> DateTimeField)

--- screenshot/models.py
+++ screenshot/models.py
@@ -4,7 +4,7 @@ from django.db import models
 # Create your models here.
 
 class Screenshot(models.Model):
-    datetime = models.DateField(auto_now=True)
+    datetime = models.DateTimeField(auto_now=True)
     image = models.ImageField(upload_to='images/')
     #
     # def __str__(self):
