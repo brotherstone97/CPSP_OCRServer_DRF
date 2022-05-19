ea7487d60c56cfcfbc8727b698e2894f3b0283cc
brotherstone97 <brotherstone97@gmail.com>
2022-05-19 20:28:48 +0900

    Limit file size

--- screenshot/views.py
+++ screenshot/views.py
@@ -17,14 +17,20 @@ def upload_screenshot(request):
             return
         image_dict = request.FILES
         for value in image_dict.getlist('files'):
-            # 확장자 제한(png, jpg, jpeg)
-            if '.png' in str(value) or '.jpg' in str(value) or '.jpeg' in str(value):
-                print(value)
-                ss = Screenshot()
-                ss.image = value
-                ss.save()
+            print(f'value.size: {value.size / 1000000}mb ')
+            # 50mb이하의 파일로 제한
+            if value.size < 52428800:
+                # 확장자 제한(png, jpg, jpeg)
+                if '.png' in str(value) or '.jpg' in str(value) or '.jpeg' in str(value):
+                    print(value)
+                    ss = Screenshot()
+                    ss.image = value
+                    ss.save()
+                else:
+                    print("value's type: ", type(str(value)))
+                    print("value: ", value)
+                    print("this file isn't contain png | jpg | jpeg")
             else:
-                print("value's type: ", type(str(value)))
-                print("value: ", value)
-                print("this file isn't contain png | jpg | jpeg")
+                print("50mb 초과")
+
     return Response(status=204)
