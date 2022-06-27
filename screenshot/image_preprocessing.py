e1234acfbd105c43a8d2a9d84a2448cc9fc4a04f
brotherstone97 <brotherstone97@gmail.com>
2022-06-28 00:07:17 +0900

    Fix: #8 Update new_file naming function

--- screenshot/image_preprocessing.py
+++ screenshot/image_preprocessing.py
@@ -1,13 +1,45 @@
 # PIL
 from PIL import Image
 import os
+from datetime import datetime
+
 
 def reduce_size(temp_path, new_path, filename):
     # typecast
-    filename = str(filename)
-    file = temp_path + filename
-    img = Image.open(file)
-    # optimizing 후 이미지 재저장
-    img.save(os.path.join(new_path, filename), optimize=True)
-    # 기존 이미지 삭제
-    os.remove(file)
+    old_filename = str(filename)
+
+    # 기존 이미지 파일명
+    temp_file = temp_path + old_filename
+    img = Image.open(temp_file)
+    # 새로 저장할 파일명
+    new_filename = get_new_filename(old_filename)
+    # 새로 저장할 파일의 최종 경로(새 디렉토리 + 새 파일명)
+    final_file_path = os.path.join(new_path, new_filename)
+    extra_int = 1
+    # 이미 파일명이 존재할 경우 존재하지 않은 이름이 나올 때까지 무한루프(요청이 초단위까지 같을 경우를 고려)
+    while True:
+        # 파일명이 중복일 때
+        if os.path.isfile(final_file_path):
+            new_filename = get_new_filename(old_filename, f'({extra_int})')
+            final_file_path = os.path.join(new_path, new_filename)
+            # 붙여줄 번호 + 1
+            extra_int += 1
+        else:
+            # optimizing 후 이미지 재저장
+            img.save(fp=final_file_path, optimize=True)
+            # 기존 이미지 삭제
+            os.remove(temp_file)
+            break
+
+#저장된 시간이 적힌 새 이름으로 바꾸기 위한 함수
+def get_new_filename(old_filename, extra_str=''):
+    # 현재 시각
+    datetime_now = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    # .이 나오는 위치
+    index_of_dot = old_filename.index('.')
+    # 확장자 추출
+    file_extension = old_filename[index_of_dot:]
+    new_filename = datetime_now + extra_str + file_extension
+
+    #파일명 예) 2022-06-28-00-00-00.jpg
+    return new_filename
